3fb93eaf3697155a23c74386ac6d967bf2c27ca3
munozjuan <munoz.juan07@hotmail.com>
2021-03-14 19:00:48 -0400

    adding logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from "react";
+import logo from "../logo.png";
 
 const Navbars = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-  <a className="navbar-brand" href="#">Navbar</a>
+  <a className="navbar-brand" href="#"><img src={logo} alt="logo..."></img></a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
