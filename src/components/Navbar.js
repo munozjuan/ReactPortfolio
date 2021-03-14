0bd785aea80ec3bc1578ad5a4ddfac47b93f73cb
munozjuan <munoz.juan07@hotmail.com>
2021-03-14 19:09:35 -0400

    sizing logo

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import logo from "../logo.png";
 const Navbars = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
-  <a className="navbar-brand" href="#"><img src={logo} alt="logo..."></img></a>
+  <a className="navbar-brand" href="#"><img className="logo"src={logo} alt="logo..."></img></a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
