0e1b74d1dd7eff9ed142e11c663a459ba59cf2f8
munozjuan <munoz.juan07@hotmail.com>
2021-03-14 22:45:31 -0400

    working on buttons

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,20 @@
-import React from "react"
+import React from "react";
+import Typed from "react-typed";
 
 const Header = () => {
     return (
         <div className="header-wraper">
             <div className="main-info">
                 <h1>Web Developer and website promotions</h1>
+                <Typed
+                    className="typed-text"
+                    strings={["Web Development", "Web Design", "Full Stack" ]}
+                    typeSpeed={40}
+                    backSpeed={60}
+                    loop
+                />
+                <a href="#" className="btn-main-offer">Contact Me</a>
+
             </div>
         </div>
     )
