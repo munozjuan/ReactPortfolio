29ea636d6bd171936c4cc496a3af4eb5340c7508
munozjuan <munoz.juan07@hotmail.com>
2021-03-14 20:49:41 -0400

    adding container

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,10 @@ import logo from "../logo.png";
 const Navbars = () => {
     return (
         <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+  <div className="container">
+
+
+
   <a className="navbar-brand" href="#"><img className="logo"src={logo} alt="logo..."></img></a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
@@ -20,6 +24,8 @@ const Navbars = () => {
       
     </ul>
     
+  </div>
+
   </div>
 </nav>
     )
